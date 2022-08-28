8fd58d4827de94035ea60cc0d0be83733932696c
MayraMendoza <mayra601m@gmail.com>
2022-08-27 23:20:40 -0400

    update

--- src/com/careerdevs/HackerRank30DaysOfCode/Day6.java
+++ src/com/careerdevs/HackerRank30DaysOfCode/Day6.java
@@ -1,5 +1,10 @@
 package com.careerdevs.HackerRank30DaysOfCode;
 
+import java.util.Scanner;
+
 public class Day6 {
     //Day 6 hacker rank
+    Scanner input = new Scanner(System.in);
+    String Word = input.next();
+
 }
