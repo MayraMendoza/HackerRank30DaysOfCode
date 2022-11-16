19792f16a1bd949b9a1b39a4c63f463d1406f8f7
MayraMendoza <mayra601m@gmail.com>
2022-11-15 21:21:31 -0500

    test

--- src/com/careerdevs/HackerRank30DaysOfCode/Main.java
+++ src/com/careerdevs/HackerRank30DaysOfCode/Main.java
@@ -9,9 +9,15 @@ public class Main {
 //        ThisClass thatclass = new ThisClass(1,0);
 //        System.out.print(thatclass);
 
-        ThisClass result;
-        result = new ThisClass(1,2);
-        System.out.println(result);
+//        ThisClass result;
+//        result = new ThisClass(1,2);
+//        System.out.println(result);
+
+        int number5= 010;
+        int number6= 040;
+
+        System.out.println(number5);
+        System.out.println(number6);
 
 
 
